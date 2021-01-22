d43b5eb06d3342f6e45b1ebbcb76c4597597c84c
Sakibul Islam <sakibul962@gmail.com>
2021-01-22 16:34:42 +0600

    created a users list

---
+++ src/people.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    displayName: 'Harry Potter',
+    userName: 'harry',
+    nickname: 'The Chosen One',
+    photoURL: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/Harry_Potter_character_poster.jpg/220px-Harry_Potter_character_poster.jpg",
+    bio: 'leader of Dumbledore\'s Army.',
+    born: '31 July 1980'
+  },
+  {
+    displayName: 'Hermione Granger',
+    userName: 'heimione',
+    nickname: 'Hermione',
+    photoURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Hermione_Granger_poster.jpg/220px-Hermione_Granger_poster.jpg',
+    bio: 'Prefect and co-founder of Dumbledore\'s Army.',
+    born: '19 September 1979'
+  },
+  {
+    displayName: 'Luna Lovegood',
+    userName: 'luna',
+    nickname: 'Loony',
+    photoURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/LunaLovegood.jpeg/220px-LunaLovegood.jpeg',
+    bio: ' Member of Dumbledore\'s Army',
+    born: '13 February 1981'
+  },
+  {
+    displayName: 'Ronald Weasley',
+    userName: 'ron',
+    nickname: 'Ron',
+    photoURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg',
+    bio: 'Gryffindor Quidditch Keeper, prefect, and co-founder of Dumbledore\'s Army',
+    born: '1 March 1980'
+  },
+  {
+    displayName: 'Ginny Weasley',
+    userName: 'ginny',
+    nickname: 'Ginny',
+    photoURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/Ginny_Weasley_poster.jpg/220px-Ginny_Weasley_poster.jpg',
+    bio: 'Quidditch Seeker and Chaser, and member of Dumbledore\'s Army',
+    born: '11 August 1981'
+  },
+  {
+    displayName: '',
+    userName: '',
+    nickname: '',
+    photoURL: '',
+    bio: '',
+    born: ''
+  }
+]

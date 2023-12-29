3b4abe3891dc91044fd981154edbe3475c14d982
sakibul-islam <sakibul962@gmail.com>
2023-12-29 19:29:39 +0600

    Fixed comment-input alignment

--- src/Components/comment/write-comment/write-comment.component.jsx
+++ src/Components/comment/write-comment/write-comment.component.jsx
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import CurrentUsersProfilePic from '../../current-user/current-users-profile-pic/current-users-profile-pic.component';
 import HoverButton from '../../hoverButton/hover-button.component';
 import { SendIcon } from '../../icons/icons';
 import './write-comment.styles.scss';
 
-const WriteComment = () => (
-  <div className='write-comment'>
-    <CurrentUsersProfilePic />
-    <div className='input'>
-      <textarea type='text' placeholder='Write a comment...'/>
-      <div className='send-button'>
-        <HoverButton>
-          <SendIcon/>
-        </HoverButton>
+const WriteComment = () => {
+  const [rows, setRows] = useState(1);
+
+  function onChange({ target: { value } }) {
+    const lines = value.split("\n").length;
+    setRows(lines);
+  }
+
+  return (
+    <div className='write-comment'>
+      <CurrentUsersProfilePic />
+      <div className='input'>
+        <textarea rows={rows} onChange={onChange} type='text' placeholder='Write a comment...' />
+        <div className='send-button'>
+          <HoverButton>
+            <SendIcon />
+          </HoverButton>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default WriteComment;
